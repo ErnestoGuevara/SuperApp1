13c0ce8ffa8ff606d24415c6595d3d509a6a40c0
FernandoFigueroa14 <luisfersurfer14@hotmail.com>
2021-10-22 15:10:13 -0500

    Test

--- App.js
+++ App.js
@@ -5,12 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
       <Text>Hola putas! :D</Text>
-=======
-      <Text>Hola putas!</Text>
-      
->>>>>>> bb9d96ce06d3cfec8615e17c8f96a7049d57f409
       <StatusBar style="auto" />
     </View>
   );
