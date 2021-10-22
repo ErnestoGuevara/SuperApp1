22c9dc5c4849434457afea46f09dac6703749782
Ernesto Guevara <30807748+ErnestoGuevara@users.noreply.github.com>
2021-10-22 15:07:27 -0500

    Cambio estupido

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola putas!</Text>
+      <Text>Hola putas! :D</Text>
       <StatusBar style="auto" />
     </View>
   );
